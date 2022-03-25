5b2df6fea0f3a4de5ca6931a0444d312e81eee4d
felixlohmeier <mail@felixlohmeier.de>
2022-03-25 22:02:28 +0000

    shfmt and shebang

--- src/initialize.sh
+++ src/initialize.sh
@@ -1,6 +1,3 @@
+#!/usr/bin/env bash
 ## Code here runs inside the initialize() function
-## Use it for anything that you need to run before any other function, like
-## setting environment vairables:
-## CONFIG_FILE=settings.ini
-##
-## Feel free to empty (but not delete) this file.
+## Use it for anything that you need to run before any other function
