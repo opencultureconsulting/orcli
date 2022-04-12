aadad1ea6b91fd95dd7d3dcc2d489bc4008c2f66
felixlohmeier <mail@felixlohmeier.de>
2022-04-12 16:26:43 +0000

    improve logging

--- src/lib/get_csrf.sh
+++ src/lib/get_csrf.sh
@@ -5,12 +5,10 @@ function get_csrf() {
   if ! response="$(curl -fs "${OPENREFINE_URL}/command/core/get-csrf-token")"; then
     if ! response="$(curl -fs "${OPENREFINE_URL}/command/core/get-version")"; then
       error "no OpenRefine reachable/running at ${OPENREFINE_URL}"
-      exit 1
     fi
   else
     if ! [[ "${response}" == '{"token":"'* ]]; then
       error "getting CSRF token failed!"
-      exit 1
     fi
     echo "?csrf_token=$(echo "$response" | cut -d \" -f 4)"
   fi
---
+++ src/lib/logging.sh
@@ -0,0 +1,13 @@
+# print messages to STDERR
+# shellcheck shell=bash
+function error() {
+  echo >&2 "[$(date +'%Y-%m-%dT%H:%M:%S')] ERROR: $1"
+  shift
+  for msg in "$@"; do echo >&2 "$msg"; done
+  exit 1
+}
+function log() {
+  echo >&2 "[$(date +'%Y-%m-%dT%H:%M:%S')] $1"
+  shift
+  for msg in "$@"; do echo >&2 "$msg"; done
+}
