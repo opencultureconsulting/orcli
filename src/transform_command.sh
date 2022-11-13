cdd92fc1811d1908fba21e6f68004e56874a04cf
felixlohmeier <mail@felixlohmeier.de>
2022-11-13 21:43:04 +0000

    add mappings and improve error handling

--- src/transform_command.sh
+++ src/transform_command.sh
@@ -34,7 +34,22 @@ for i in "${!files[@]}"; do
         # parse one line/operation into array
         declare -A data="($(echo "$line" | jq -r 'to_entries | map("[\(.key)]=" + @sh "\(.value|tostring)") | .[]'))"
         # map operation names to command endpoints
+        # https://github.com/OpenRefine/OpenRefine/blob/master/main/webapp/modules/core/MOD-INF/controller.js
         com="${data[op]#core/}"
+        if [[ $com == "multivalued-cell-join" ]]; then com="join-multi-value-cells"; fi
+        if [[ $com == "multivalued-cell-split" ]]; then com="split-multi-value-cells"; fi
+        if [[ $com == "column-addition" ]]; then com="add-column"; fi
+        if [[ $com == "column-addition-by-fetching-urls" ]]; then com="add-column-by-fetching-urls"; fi
+        if [[ $com == "column-removal" ]]; then com="remove-column"; fi
+        if [[ $com == "column-rename" ]]; then com="rename-column"; fi
+        if [[ $com == "column-move" ]]; then com="move-column"; fi
+        if [[ $com == "column-split" ]]; then com="split-column"; fi
+        if [[ $com == "column-reorder" ]]; then com="reorder-columns"; fi
+        if [[ $com == "recon" ]]; then com="reconcile"; fi
+        if [[ $com == "extend-reconciled-data" ]]; then com="extend-data"; fi
+        if [[ $com == "row-star" ]]; then com="annotate-rows"; fi
+        if [[ $com == "row-flag" ]]; then com="annotate-rows"; fi
+        if [[ $com == "row-removal" ]]; then com="remove-rows"; fi
         if [[ $com == "row-reorder" ]]; then com="reorder-rows"; fi
         unset "data[op]"
         # rename engineConfig to engine
@@ -49,9 +64,14 @@ for i in "${!files[@]}"; do
         done)
         # get project id and csrf token; post data to it's individual endpoint
         if response="$(curl -fs --data "project=$(get_id "${args[project]}")" "${curloptions[@]}" "${OPENREFINE_URL}/command/core/${com}$(get_csrf)")"; then
-            log "applied ${com} to ${args[project]}" "Response: $(jq '.historyEntry.description' <<< "$response")"
+            response_code="$(jq -r '.code' <<<"$response")"
+            if [[ $response_code == "ok" ]]; then
+                log "transformed ${args[project]} with ${com}" "Response: $(jq -r '.historyEntry.description' <<<"$response")"
+            else
+                error "transforming ${args[project]} with ${com} from ${files[$i]} failed!" "Response: $(jq -r '.message' <<<"$response")"
+            fi
         else
-            error "applying ${com} from ${files[$i]} to ${args[project]} failed!"
+            error "transforming ${args[project]} with ${com} from ${files[$i]} failed!"
         fi
         unset data
     done
