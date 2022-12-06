a8532e9853d41a123f20f691c99df1b5f5dc0d2b
felixlohmeier <mail@felixlohmeier.de>
2022-12-06 16:21:11 +0000

    add test for info command

--- tests/info.sh
+++ tests/info.sh
@@ -9,10 +9,17 @@ trap '{ rm -rf "${tmpdir}"; }' 0 2 3 15
 # input
 cp data/example.csv "${tmpdir}/${t}.csv"
 
+# assertion
+cat << "DATA" > "${tmpdir}/${t}.assert"
+a
+b
+c
+DATA
+
 # action
 cd "${tmpdir}" || exit 1
 orcli import csv "${t}.csv"
-orcli info "${t} csv"
+orcli info "${t} csv" | jq -r .columns[] > "${t}.output"
 
 # test
-# grep "${t}" "${t}.output"
+diff -u "${t}.assert" "${t}.output"
