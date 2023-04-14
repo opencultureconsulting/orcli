98ef70e29bb13b41a61b9bf6dafb1accf17b9361
Felix Lohmeier <mail@felixlohmeier.de>
2023-04-14 12:34:04 +0200

    replace curl --retry with bash builtins (#101)

--- src/test_command.sh
+++ src/test_command.sh
@@ -34,14 +34,20 @@ OPENREFINE_PID="$!"
 # update trap to kill OpenRefine on error or exit
 trap '{ rm -rf "$OPENREFINE_TMPDIR"; rm -rf /tmp/jetty-127_0_0_1-3333*; kill -9 "$OPENREFINE_PID"; }' 0 2 3 15
 
-# wait until OpenRefine is running (timeout 15s + 15s)
-if ! curl -fs --retry 15 --retry-connrefused --retry-delay 1 "${OPENREFINE_URL}/command/core/get-version" &>/dev/null; then
-    # try again with IPv4 only
-    if ! curl -fs -4 --retry 15 --retry-connrefused --retry-delay 1 "${OPENREFINE_URL}/command/core/get-version" &>/dev/null; then
-        error "starting OpenRefine server failed!"
+# wait until OpenRefine is running (timeout 20s)
+ready="n"
+for i in {1..20}; do
+    if curl -fs "${OPENREFINE_URL}/command/core/get-version" &>/dev/null; then
+        ready="y"
+        break
+    else
+        sleep 1
     fi
-else
+done
+if [[ "$ready" == "y" ]]; then
     log "started OpenRefine with tmp workspace ${OPENREFINE_TMPDIR}"
+else
+    error "starting OpenRefine server failed!"
 fi
 
 # execute tests in subshell
