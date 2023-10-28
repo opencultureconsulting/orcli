91d42471bf8e4253910793f34b7f1640012407f2
Felix Lohmeier <mail@felixlohmeier.de>
2023-10-28 20:17:21 +0000

    import json: more tests

---
+++ tests/import-json-trimStrings.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+t="import-json-trimStrings"
+
+# create tmp directory
+tmpdir="$(mktemp -d)"
+trap '{ rm -rf "${tmpdir}"; }' 0 2 3 15
+
+# input
+cat << "DATA" > "${tmpdir}/${t}.json"
+[
+    {
+      "a": 1,
+      "b": 2,
+      "c": 3
+    },
+    {
+      "a": 0,
+      "b": " 0",
+      "c": "0 "
+    },
+    {
+      "a": "$",
+      "b": "\\",
+      "c": "'"
+    }
+]
+DATA
+
+# assertion
+cat << "DATA" > "${tmpdir}/${t}.assert"
+_ - a	_ - b	_ - c
+1	2	3
+0	0	0
+$	\	'
+DATA
+
+# action
+cd "${tmpdir}" || exit 1
+orcli import json "${t}.json" --projectName "${t}" --trimStrings
+orcli export tsv "${t}" > "${t}.output"
+
+# test
+diff -u "${t}.assert" "${t}.output"
