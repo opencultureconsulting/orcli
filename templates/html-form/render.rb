b89331fa7176a6c4cdfb1c7509e9baae83618688
Felix Lohmeier <mail@felixlohmeier.de>
2025-10-10 10:23:37 +0000

    html-form docs multiple files instead of one

--- templates/html-form/render.rb
+++ templates/html-form/render.rb
@@ -6,8 +6,13 @@ require 'fileutils'
 template = "#{source}/html-form.gtx"
 gtx = GTX.load_file template
 
-# Render the file
+# Render the file for the main command
 save "#{target}/index.html", gtx.parse(command)
+
+# Render a file for each subcommand
+command.deep_commands.reject(&:private).each do |subcommand|
+  save "#{target}/#{subcommand.full_name}.html", gtx.parse(subcommand)
+end
 
 # Copy JS and CSS to target directory
 FileUtils.cp "#{source}/style.css", "#{target}/style.css"
